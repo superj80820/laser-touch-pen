05380506fc8e6196dbfd1611d58954be5f10fda6
York Lin <superj80820@gmail.com>
2019-02-10 02:31:21 +0800

    Add matlab call python funtion (Issue_1)

---
+++ model/handDetection.py
@@ -0,0 +1,13 @@
+import sys, os
+
+class handDetection(object):
+    def __init__(self, image_path):
+        self.image_path = image_path
+
+    def detection(self):
+        """
+        請在這裡新增yolo function
+        如果偵測到手 請回傳手的類型
+        如果沒有偵測到 請回傳False
+        """
+        return True
