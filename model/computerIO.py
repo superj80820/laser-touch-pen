30855e735ea9542ca0a404e707cf1eb319ecc99c
superj80820 <superj80820@gmail.com>
2019-03-29 21:12:16 +0800

    Move websocketClient.py to line_discuss repository (Issue-27)

--- model/computerIO.py
+++
@@ -1,9 +0,0 @@
-import pyscreenshot as ImageGrab
-
-class computerIO(object):
-    def __init__(self):
-        pass
-
-    def screenshop(self):
-        image = ImageGrab.grab()
-        return image
